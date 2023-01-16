ef7a9fa6495aa740a4d25e40bc0dcf2df4d0fa4d
jordi-marigot <jmarigot@alum.joviat.cat>
2023-01-16 13:28:35 +0100

    funciona xd

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var index = express();index.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World!    owo');
 });index.listen(3000, function () {
   console.log('hola owo 3000!');
 });
