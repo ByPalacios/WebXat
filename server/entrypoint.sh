87dac6031fa2c1e56cf973b15e976e21ac2ad774
jordi-marigot <jmarigot@alum.joviat.cat>
2023-01-18 11:58:59 +0100

    merge

---
+++ server/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+rm -R node_modules 
+rm package-lock.json 
+npm install -S express
+npm install -gS nodemon
+npm install -S socket.io
+npm install -S mysql
+npm install -S cookie-parser
+npm install -S express-session
+npm install
+nodemon index.js
