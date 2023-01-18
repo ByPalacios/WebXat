50cba4de101b42b65659af0c6a4f222e81b9a46c
jordi-marigot <jmarigot@alum.joviat.cat>
2023-01-18 12:19:38 +0100

    fix run.py

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ if system == "Windows":
     info = os.system("echo ---------------------------------------------")
     info = os.system("echo Build socket.io-server:")
     info = os.system("echo ---------------------------------------------")
-    command1 = os.system("cd node && docker build . -t node:staging --no-cache")
+    command1 = os.system("cd node & docker build . -t node:staging --no-cache")
     command2 = os.system("cd ..")
     #command1 = os.system("docker load -i node.tar")
     command1 = os.system("cd server & docker build . -t socket.io-server:staging --no-cache")
