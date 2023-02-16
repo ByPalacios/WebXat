e14624863a24adbbbded01c78e040bf569831bf0
Pau Palacios <paupalacioseso@gmail.com>
2023-02-16 13:25:34 +0100

    Tweaks

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <h1>WebXat Ruine TT</h1>
       <section className="FormDiv">
-          <h3 className='formTitles'>Username:</h3>
+          <h1 className={'formTitle'}>WebXat Ruine TT</h1>
         <input className='formInput' type={Text} placeholder="Escriu el teu Username" id="username"></input>
-          <h3 className='formTitles'>Email:</h3>
         <input className='formInput' type={Text} placeholder="Escriu el teu Email" id="email"></input>
-          <h3 className='formTitles'>Password:</h3>
         <input className='formInput' type={Text} placeholder="Escriu la teva contrasenya" id="password"></input>
-        </section>
+          <input className='formInput' type={Text} placeholder="Torna a escriure la teva contrasenya" id="password"></input>
+        <input className={'formButton'} type={"submit"} value={'Registrar-se'}></input>
+      </section>
     </div>
   );
 }
