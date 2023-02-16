a9a23db76f1613f3c5f248cd45f9a10101f8a2c6
Pau Palacios <paupalacioseso@gmail.com>
2023-02-16 12:59:34 +0100

    Form bonito

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ const App = () => {
     <div className="App">
       <h1>WebXat Ruine TT</h1>
       <section className="FormDiv">
-        <h3>Username:</h3>
+          <h3 className='formTitles'>Username:</h3>
         <input className='formInput' type={Text} placeholder="Escriu el teu Username" id="username"></input>
-        <h3>Email:</h3>
+          <h3 className='formTitles'>Email:</h3>
         <input className='formInput' type={Text} placeholder="Escriu el teu Email" id="email"></input>
-        <h3>Password:</h3>
+          <h3 className='formTitles'>Password:</h3>
         <input className='formInput' type={Text} placeholder="Escriu la teva contrasenya" id="password"></input>
         </section>
     </div>
