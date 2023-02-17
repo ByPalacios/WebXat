74f631fac3c37bc81bea9b45b6b82518f2324775
Pau Palacios <paupalacioseso@gmail.com>
2023-02-17 08:09:12 +0100

    Added Terms & Conditions

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const App = () => {
         <input className='formInput' type={Text} placeholder="Escriu el teu Email" id="email"></input>
         <input className='formInput' type={Text} placeholder="Escriu la teva contrasenya" id="password"></input>
           <input className='formInput' type={Text} placeholder="Torna a escriure la teva contrasenya" id="password"></input>
+          <p className={"formCondicions"}>Accepta els <a href={"google.com"}>Terminis i condicions</a></p>
         <input className={'formButton'} type={"submit"} value={'Registrar-se'}></input>
       </section>
     </div>
