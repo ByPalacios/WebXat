3a1d091003654c788025f6b0789ad73a2344ef12
Jordi Marigot <98736519+jordi-marigot@users.noreply.github.com>
2023-02-20 22:05:08 +0100

    Update run.py

--- run.py
+++ run.py
@@ -24,11 +24,10 @@ if platform.system() == "Windows":
         open_file.write(content)
     
     info = os.system("echo ---------------------------------------------")
-    info = os.system("echo Build socket.io-server:")
+    info = os.system("echo Build Node Image:")
     info = os.system("echo ---------------------------------------------")
     command1 = os.system("cd node & docker build . -t node:staging --no-cache")
     command2 = os.system("cd ..")
-    #command1 = os.system("docker load -i node.tar")
     command1 = os.system("cd server & docker build . -t socket.io-server:staging --no-cache")
     info = os.system("echo ---------------------------------------------")
     info = os.system("echo Build socket.io-client:")
