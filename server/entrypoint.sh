95f9c2dafc84ce36a1d1cfaaf160586c81eba704
Pau Palacios <paupalacioseso@gmail.com>
2023-02-27 09:53:35 +0100

    dsfs

---
+++ server/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+rm -R node_modules 
+rm package-lock.json 
+npm install -S express
+npm install -gS nodemon
+npm install -S socket.io
+npm install -S mysql2
+npm install -S cookie-parser
+npm install -S express-session
+npm install
+nodemon --legacy-watch index.js
