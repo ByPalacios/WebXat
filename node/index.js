4bb0065e607e148c348a8e11a82cb8154ee63af9
Pau Palacios <paupalacioseso@gmail.com>
2023-02-27 09:38:28 +0100

    Changes

--- node/index.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var index = express();index.get('/', function (req, res) {
-  res.send('Hello World!    98999999');
-});index.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
